fa19fa29e6f580d011a37539b4d4e1689e4febd1
gota_morishita <optimistic.gota@gmail.com>
2018-08-22 13:03:44 +0900

    implemented reverse option

--- cpp/insert_sort.cpp
+++ cpp/insert_sort.cpp
@@ -2,21 +2,29 @@
 #include <cassert>
 #include <array>
 
-void insert_sort(int a[], int len) {
+void insert_sort(int a[], int len, bool reverse = false) {
     for ( int j = 1; j < len; j++ ) {
         int key = a[j];
         int i = j - 1;
-        while ( i >= 0 && a[i] > key ) {
-            a[i+1] = a[i];
-            i--;
+        if ( reverse ) {
+            while ( i >= 0 && a[i] < key ) {
+                a[i+1] = a[i];
+                i--;
+            }
+            a[i+1] = key;
+        } else {
+            while ( i >= 0 && a[i] > key ) {
+                a[i+1] = a[i];
+                i--;
+            }
+            a[i+1] = key;
         }
-        a[i+1] = key;
     }
 }
 
 int main() {
-    int a[] = {5, 9, 3, 2, 1};
-    int n = 5;
+    int a[] = {5, 9, 3, 2, 7, 1, 10};
+    int n = 7;
     insert_sort(a, n);
     for ( int i = 0; i < n; i ++ ) {
         std::cout << a[i] << " ";
