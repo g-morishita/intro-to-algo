9557d1543d4a18711adad7983cec352c1caa67d9
gota_morishita <optimistic.gota@gmail.com>
2018-08-24 12:24:43 +0900

    implementing the algorithm to count inversions

---
+++ cpp/2/count_inversions.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+typedef std::vector<int> int_vec; 
+
+int count_inversions(int_vec a, int p, int q, int r) {
+}
+
+int merge_inversions(int_vec a, int p, int q) {
+    int m = ( p + q ) / 2;
+    
+}
